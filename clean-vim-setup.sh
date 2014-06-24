832bb86ce841e66eb3afa467c9ece5ed4bbc6cb5
Robin Galloway-Lunn <robingl@aecher.com>
2014-06-24 10:41:51 +0100

    Reordered some git clone commands. Added vim-commentary.

--- clean-vim-setup.sh
+++ clean-vim-setup.sh
@@ -20,15 +20,16 @@ mkdir -p ~/.vim/bundle
 
 echo Cloning bundle git repos.
 cd ~/.vim/bundle
-git clone git@github.com:jordwalke/flatlandia.git
-git clone git@github.com:scrooloose/nerdtree.git
-# git clone https://github.com/vim-scripts/paredit.vim
-git clone git@github.com:bling/vim-airline.git
+git clone git@github.com:tpope/vim-pathogen.git
 git clone git@github.com:kien/rainbow_parentheses.vim.git
 git clone git://github.com/tpope/vim-classpath.git
-git clone git@github.com:guns/vim-clojure-static.git
 git clone git://github.com/tpope/vim-fireplace.git
-git clone git@github.com:tpope/vim-pathogen.git
+git clone git://github.com/tpope/vim-commentary.git
+git clone git@github.com:scrooloose/nerdtree.git
+git clone git@github.com:guns/vim-clojure-static.git
+# git clone https://github.com/vim-scripts/paredit.vim
+git clone git@github.com:jordwalke/flatlandia.git
+git clone git@github.com:bling/vim-airline.git
 
 echo Fetching .vimrc from git
 curl -LSso ~/.vimrc https://github.com/robingl/vim-config/raw/master/vimrc
