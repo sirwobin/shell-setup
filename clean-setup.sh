0ed858e26680482cb2adc435ef6743e8d0a05e14
Robin Galloway-Lunn <robingl@aecher.com>
2014-11-29 18:34:44 +0000

    Adding vim-sexp and friends.

--- clean-setup.sh
+++ clean-setup.sh
@@ -25,6 +25,9 @@ git clone git@github.com:kien/rainbow_parentheses.vim.git
 git clone git://github.com/tpope/vim-classpath.git
 git clone git://github.com/tpope/vim-fireplace.git
 git clone git://github.com/tpope/vim-commentary.git
+git clone git@github.com:tpope/vim-repeat.git
+git clone git@github.com:guns/vim-sexp.git
+git clone git@github.com:tpope/vim-sexp-mappings-for-regular-people.git
 git clone git@github.com:scrooloose/nerdtree.git
 git clone git@github.com:guns/vim-clojure-static.git
 # git clone https://github.com/vim-scripts/paredit.vim
