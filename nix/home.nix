53f76d58ff1c770b73e2e7687f0345e67c69577d
robin <robin108@gmail.com>
2022-08-11 09:42:52 +0000

    Zsh default shell now used correctly inside nix-shell.

--- nix/home.nix
+++ nix/home.nix
@@ -15,6 +15,18 @@
     enableSyntaxHighlighting = true;
     history.share = false;
 
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "lein" ];
