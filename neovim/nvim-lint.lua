ed6f2c684c8773657099f548cf60170815cdf13b
robin <robin108@gmail.com>
2022-10-27 15:27:09 +0200

    Only apply the linter to clojure files.

--- neovim/nvim-lint.lua
+++ neovim/nvim-lint.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged" }, {
+  pattern = {"*.clj", "*.cljs", "*.cljc", "*.edn"},
   callback = function()
     require("lint").try_lint()
   end,
