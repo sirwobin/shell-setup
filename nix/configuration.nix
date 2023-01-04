30de3bfc84c1321f20a8dcf6ba6d3501564783df
robin <robin108@gmail.com>
2023-01-04 13:47:27 +0100

    Scanner, x11, user...

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+#      ./sane-extra-config.nix
     <home-manager/nixos>
   ];
 
@@ -20,4 +21,45 @@
   # Possibly also VirtualBox guest additions: https://nixos.wiki/wiki/VirtualBox
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.x11 = true;
+
+  # Enable scanner support,
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.addresses = true;
+  services.avahi.publish.userServices = true;
+
+  # hardware.sane.extraConfig."pixma" = ''
+    # bjnp://192.168.2.24
+  # '';
+
+  # Use LightDM for login and no desktop manager
+  services.xserver.displayManager.defaultSession = "none+i3";
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+
+  # Define a user account.
+  users.users.robin = {
+    isNormalUser = true;
+    description = "Robin L";
+    extraGroups = [ "networkmanager" "wheel" "scanner" "lp" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      firefox
+      chromium
+      bitwarden
+      bitwarden-cli
+      kitty
+      xsane
+    ];
+  };
+
+  # Enable automatic login.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "robin";
+
+  # Allow Canon scanner to be detected.
+  networking.firewall.allowedTCPPorts = [ 8612 ];
 }
