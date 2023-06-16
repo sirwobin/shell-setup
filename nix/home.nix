ad6e551f9379c9fd0fc3bc81ee0a81d0edaf6c89
robin <robin108@gmail.com>
2023-06-16 14:00:56 +0200

    Upgrade zsh-nix-shell.

--- nix/home.nix
+++ nix/home.nix
@@ -22,15 +22,15 @@
     enableAutosuggestions = true;
     history.share = false;
 
-    plugins = [
+    plugins = [ 
       {
         name = "zsh-nix-shell";
         file = "nix-shell.plugin.zsh";
         src = pkgs.fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.5.0";
-          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          rev = "v0.7.0";
+          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
         };
       }
     ];
