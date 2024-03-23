efc06063779871d902567d866f07caa8c31a036f
robin <robin108@gmail.com>
2024-03-23 17:00:50 +0100

    Updates

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,8 +1,62 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+
 {
-  imports = [
-#      ./sane-extra-config.nix
-    <home-manager/nixos>
-  ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-fe63103a-dc2b-48ab-84bc-0b6cb03db3f8".device = "/dev/disk/by-uuid/fe63103a-dc2b-48ab-84bc-0b6cb03db3f8";
+  boot.initrd.luks.devices."luks-fe63103a-dc2b-48ab-84bc-0b6cb03db3f8".keyFile = "/crypto_keyfile.bin";
+
+  networking.hostName = "rl-nixtop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Local hostname lookups
+  networking.extraHosts = ''
+    192.168.2.10 pi.hole
+  '';
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
 
   # Enable nix flakes
   nix.package = pkgs.nixFlakes;
@@ -14,48 +68,65 @@
   services.udisks2.enable = true;
   services.blueman.enable = true;
   services.onedrive.enable = true;
-  programs.steam.enable = true;
-
   programs.zsh.enable = true;
-  # users.extraUsers.nixos.shell = pkgs.zsh;
-  # or
-  # users.users.robin.shell = pkgs.zsh;
+  # programs.steam.enable = true;
 
-  i18n.defaultLocale = "en_GB.UTF-8";
-  #services.localtimed.enable = true;
-  time.timeZone = "Europe/Amsterdam";
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-  # Possibly also VirtualBox guest additions: https://nixos.wiki/wiki/VirtualBox
-  # virtualisation.virtualbox.guest.enable = true;
-  # virtualisation.virtualbox.guest.x11 = true;
+  # Use LightDM for login and no desktop manager
+  services.xserver.displayManager.defaultSession = "none+i3";
+  services.xserver.windowManager.i3.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable scanner support,
-  hardware.sane.enable = true;
-  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-  services.avahi.publish.enable = true;
-  services.avahi.publish.addresses = true;
-  services.avahi.publish.userServices = true;
-
-  # hardware.sane.extraConfig."pixma" = ''
-    # bjnp://192.168.2.24
-  # '';
+  # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   fonts.packages = with pkgs; [
     fantasque-sans-mono
     (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
   ];
 
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "cnijfilter2" ];
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.cnijfilter2 ];
+
+  # Enable scanner support,
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+  services.avahi.enable = true;
+  services.avahi.nssmdns4 = true;
+  services.avahi.publish.enable = true;
+  services.avahi.publish.addresses = true;
+  services.avahi.publish.userServices = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
-
-  # Use LightDM for login and no desktop manager
-  services.xserver.displayManager.defaultSession = "none+i3";
-  services.xserver.windowManager.i3.enable = true;
 
   # Enable yubikey services and programs
   services.udev.packages = [ pkgs.yubikey-personalization ];
@@ -73,51 +144,63 @@
     services.sudo.u2fAuth = true;
   };
 
-  # Define a user account.
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.robin = {
     isNormalUser = true;
-    description = "Robin L";
+    description = "Robin Lunn";
     extraGroups = [ "networkmanager" "wheel" "audio" "scanner" "lp" "vboxusers" ];
     shell = pkgs.zsh;
   };
 
-  # Enable automatic login.
+  # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "robin";
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
-  environment.localBinInPath = true;
+  # Allow unfree packages
+  # unstable = import <unstable> { config.allowUnfree = true; };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     dunst
     wireguard-tools
     networkmanagerapplet
     blueman
   ];
+  environment.localBinInPath = true;
 
-#  environment.etc = {
-#    "wireguard/ivpn_nl3.conf" = {
-#      text = ''
-#      [Interface]
-#      Address = 172.30.245.78/32
-#      DNS = 172.16.0.1
-#      PrivateKey = <substitute from file>
-#      ListenPort = 2049
-#      [Peer]
-#      PublicKey = XDU6Syq1DY82IMatsHV0x/TAtbLiRwh/SdFCXlEn40c=
-#      Endpoint = nl3.wg.ivpn.net:2049
-#      AllowedIPs = 0.0.0.0/0
-#      '';
-#      mode = "0400";
-#    };
-#  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-  # Allow Canon scanner to be detected.
-  networking.firewall.allowedTCPPorts = [ 8612 ];
-  # Wireguard port.
-  # networking.firewall.allowedUDPPorts = [ 2049 ];
-
+  # List services that you want to enable:
   # Enable virtualbox. Ref <https://nixos.wiki/wiki/Virtualbox>
   virtualisation.virtualbox.host.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # Allow Canon scanner to be detected.
+  networking.firewall.allowedTCPPorts = [ 8612 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
 }
